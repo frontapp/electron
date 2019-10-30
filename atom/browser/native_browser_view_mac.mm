39e1ad79002e71fa52cd7a72bf3d4588feb50589
Jeremy Spiegel <jeremy.spiegel@frontapp.com>
2019-10-29 19:03:03 -0700

    Fix choppy macOS drag

--- atom/browser/native_browser_view_mac.mm
+++ atom/browser/native_browser_view_mac.mm
@@ -42,7 +42,7 @@ const NSAutoresizingMaskOptions kDefaultAutoResizingMask =
 }
 
 - (void)mouseDown:(NSEvent*)event {
-  if ([self.window respondsToSelector:@selector(performWindowDragWithEvent)]) {
+  if ([self.window respondsToSelector:@selector(performWindowDragWithEvent:)]) {
     // According to Google, using performWindowDragWithEvent:
     // does not generate a NSWindowWillMoveNotification. Hence post one.
     [[NSNotificationCenter defaultCenter]
@@ -63,7 +63,7 @@ const NSAutoresizingMaskOptions kDefaultAutoResizingMask =
 }
 
 - (void)mouseDragged:(NSEvent*)theEvent {
-  if ([self.window respondsToSelector:@selector(performWindowDragWithEvent)]) {
+  if ([self.window respondsToSelector:@selector(performWindowDragWithEvent:)]) {
     return;
   }
 
